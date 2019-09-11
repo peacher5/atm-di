e524789cf46c9b9cd5078f6ddbd146e03554333f
Peacher W <peacher5@hotmail.com>
2019-09-11 15:20:14 +0700

    Add all hw tasks

---
+++ src/main/java/atmannotation/Main.java
@@ -0,0 +1,12 @@
+package atmannotation;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("config-annotation.xml");
+        ATMSimulator atmSimulator = context.getBean(ATMSimulator.class);
+        atmSimulator.run();
+    }
+}
